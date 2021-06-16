b2ea9a16dbef1e22e4f2f74f5d60ae3cd2b72b44
Regy <j.reg1006@gmail.com>
2021-06-16 17:53:54 -0400

    Add model

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+        image_cat = models.ImageField()
+        name_cat = models.CharField(max_length=30)
+        ascii_cat = models.TextField()
+        title = models.CharField(max_length=200)
+        
+        def publish(self):
+            self.save()
+
+        def __str__(self):
+            return self.title
