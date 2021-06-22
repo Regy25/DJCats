a0919ed245824aec95bdc0716900295ab4eb7b7e
Josue Ramos <josu.ramos@duocuc.cl>
2021-06-22 18:37:59 -0400

    cambios en forms
    Cambios, add_cat y htmls

--- cats/forms.py
+++ cats/forms.py
@@ -15,6 +15,30 @@ class CatsForm(forms.ModelForm):
         }
 
         widgets = {
+            'name_cat': forms.TextInput(attrs={'class': 'form-control','id':'name_f'}),
+            'ascii_cat': forms.Textarea(attrs={'class': 'form-control','id':'ascii_f'}),
+            'desc_cat': forms.Textarea(attrs={'class': 'form-control','id':'desc_f'}),
+            'imagen_cat': forms.FileInput(attrs={'class': 'form-control','id':'imagen_f'}),
+        }
+
+#PorSiAcaso
+
+
+class CatsFormValidate(forms.ModelForm):
+    class Meta:
+        model = Cats
+        fields = ['id','name_cat', 'ascii_cat', 'desc_cat', 'imagen_cat']
+
+        labels = {
+            'id': 'id',
+            'name_cat': 'Nombre',
+            'ascii_cat': 'Gato',
+            'desc_cat': 'Descripcion',
+            'imagen_cat': 'Imagen',
+        }
+
+        widgets = {
+            'id' : forms.NumberInput(attrs={'class': 'form-control'}),
             'name_cat': forms.TextInput(attrs={'class': 'form-control'}),
             'ascii_cat': forms.Textarea(attrs={'class': 'form-control'}),
             'desc_cat': forms.Textarea(attrs={'class': 'form-control'}),
